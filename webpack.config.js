184d448ae468e1fc2c4efff3e5cb1f0a24676f7a
vladislav-tarasov <vladtarasov306@gmail.com>
2022-09-12 18:27:20 +0300

    added rules to eslint

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       {
         test: /\.(css|s[a|c]ss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
